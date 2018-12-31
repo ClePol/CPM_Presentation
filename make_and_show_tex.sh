c3b827e96aa685ad3049390d26a115fa6824b914
Clemens <clemens.pollak@gmail.com>
2018-12-31 18:22:03 +0100

    added bibtex

--- make_and_show_tex.sh
+++ make_and_show_tex.sh
@@ -1,6 +1,9 @@
 # bin/bash/
 
 /Library/TeX/texbin/pdflatex ausarbeitung.tex
+/Library/TeX/texbin/bibtex ausarbeitung.aux
+/Library/TeX/texbin/pdflatex ausarbeitung.tex
+/Library/TeX/texbin/pdflatex ausarbeitung.tex
 open ausarbeitung.pdf
 #rm ausarbeitung.aux
 #rm ausarbeitung.log
